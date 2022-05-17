10b59f397f94bb3270b426b5035134aff71399d5
BrunMan <87078099+Tarmacshrimp@users.noreply.github.com>
2022-05-17 20:10:35 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+namespace Linked_List_testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] data = new object[] { 1, 2, 3, 4, 5 };
+
+            LinkedList linkedList = new LinkedList(data);
+            linkedList.Next();
+            Console.WriteLine(linkedList.GetCurrent());
+            linkedList.Add("hello");
+
+        }
+    }
+
+    class LinkedList
+    {
+        Node current = new Node();
+        
+        public LinkedList(object[] data)
+        {
+            Node LastNode = new Node();
+            for (int i = 0; i < data.Length; i++) 
+            {
+                Node BeingAdded = new Node();
+                if(i == 0)
+                {
+                    current.data = data[i];
+                    LastNode = current;
+                    continue;
+                }           
+                
+                BeingAdded.data = data[i];
+                LastNode.head = BeingAdded;
+                LastNode = BeingAdded;
+            }          
+        }
+
+        public object GetCurrent()
+        {
+            return current.data;
+        }
+        public void Next()
+        {
+            current = current.head;
+        }
+
+        public void Add(object Data)
+        {
+            Node tempNode = new Node { data = current.data, head = current.head };
+
+            while(tempNode.head != null)
+            {
+                tempNode = tempNode.head;
+            }
+
+            tempNode.head = new Node { data = Data };
+        }
+
+        class Node
+        {
+            public object data { get; set; }
+            public Node head { get; set; }        
+        }
+    }
+    
+}
